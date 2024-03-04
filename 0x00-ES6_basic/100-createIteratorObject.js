729a0afce2114cecce9e418a7ec75a4b74d0c14e
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-03-04 10:52:43 +0000

    iterate through report object

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -2,6 +2,6 @@ export default function createIteratorObject(report) {
   const employees = []
   for (const employee of [...Object.values(report.allEmployees)]) {
     employees.push(...employee);
-}
+  }
   return employees;
 }
