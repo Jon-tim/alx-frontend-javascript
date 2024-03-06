1443e02b3c8bfbd6276406a94836e9c6baf45732
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-03-06 23:38:00 +0000

    return array of results

--- 0x01-ES6_promise/6-final-user.js
+++ 0x01-ES6_promise/6-final-user.js
@@ -2,9 +2,15 @@ import signUpUser from './4-user-promise';
 import uploadPhoto from './5-photo-reject';
 
 export default function handleProfileSignup(firstName, lastName, fileName) {
-  return Promise.allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
-    .then((results) => results.map((result) => ({
-      status: result.status,
-      value: result.status === 'fulfilled' ? result.value : result.reason,
-    })));
+  const finalUserResult = [];
+  return Promise.allSettled([
+    signUpUser(firstName, lastName),
+    uploadPhoto(fileName),
+  ]).then((results) => {
+    results.map(({ status, value, reason }) => finalUserResult.push({
+      status,
+      value: status === 'fulfilled' ? value : reason,
+    }));
+    return finalUserResult;
+  });
 }
