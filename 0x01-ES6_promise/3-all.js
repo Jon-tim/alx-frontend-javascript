9084156cc26f8087e57546b9eebfceec3aff5715
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-03-06 23:51:55 +0000

    return promise

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,7 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  Promise.all([uploadPhoto(), createUser()]).then(([photoValues, userValues]) => {
+  return Promise.all([uploadPhoto(), createUser()]).then(([photoValues, userValues]) => {
     console.log(`${photoValues.body} ${userValues.firstName} ${userValues.lastName}`);
   }).catch(() => console.log('Signup system offline'));
 }
