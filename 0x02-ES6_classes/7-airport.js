1c5ef44b18a5105d0c34561d0236d819e9228abb
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-03-07 17:31:53 +0000

    add airport class

---
+++ 0x02-ES6_classes/7-airport.js
@@ -0,0 +1,14 @@
+/* eslint-disable no-underscore-dangle */
+export default class Airport {
+  constructor(name, code) {
+    if (typeof name !== 'string') throw new TypeError('name must be a string');
+    if (typeof code !== 'string') throw new TypeError('name must be a string');
+
+    this._name = name;
+    this._code = code;
+  }
+
+  toString() {
+    return `[object ${this._code}]`;
+  }
+}
