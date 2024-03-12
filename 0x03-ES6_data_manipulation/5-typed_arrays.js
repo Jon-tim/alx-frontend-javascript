78f31440aaa86218f3d22eb8ebab98ce3620babe
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-03-12 11:18:00 +0000

    fix code

--- 0x03-ES6_data_manipulation/5-typed_arrays.js
+++ 0x03-ES6_data_manipulation/5-typed_arrays.js
@@ -4,11 +4,6 @@ export default function createInt8TypedArray(length, position, value) {
   }
   const buffer = new ArrayBuffer(length);
   const view = new DataView(buffer);
-  try {
-    view.setInt8(position, value);
-  } catch (e) {
-    throw new Error('Position outside range');
-  }
-
-  return buffer;
+  view.setInt8(position, value);
+   return buffer;
 }
