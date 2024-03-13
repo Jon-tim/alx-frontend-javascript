b2e45ecdd2e57e2db5a40524fd2491762be42b32
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-03-13 21:54:34 +0000

    class type

--- 0x04-TypeScript/task_1/js/main.ts
+++ 0x04-TypeScript/task_1/js/main.ts
@@ -18,3 +18,27 @@ interface printTeacherFunction {
 const printTeacher: printTeacherFunction = (firstName, lastName) => {
   return `${firstName[0]}. ${lastName}`;
 }
+
+interface studentClass {
+  workOnHomework(): string;
+  displayName(): string;
+}
+
+interface StudentClassConstructor {
+  new (firstName: string, lastName: string): studentClass;
+}
+
+class StudentClass implements studentClass {
+  firstName: string;
+  lastName: string;
+  constructor(firstName: string, lastName: string) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+;2  }
+  workOnHomework(): string {
+    return 'Currently working';
+  };
+ ;8 displayName(): string {
+    return this.firstName;
+  }
+}
