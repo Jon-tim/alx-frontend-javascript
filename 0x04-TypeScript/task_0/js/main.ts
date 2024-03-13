2fae4049bd2115fcd879dfddd18403940f14833f
oluwatimilehin awoniyi <awoniyitimi@outlook.com>
2024-03-13 17:47:39 +0000

    Creating an interface for a student

--- 0x04-TypeScript/task_0/js/main.ts
+++ 0x04-TypeScript/task_0/js/main.ts
@@ -1,21 +1,35 @@
 interface Student {
-	firstName: string;
-	lastName: string;
-	age: number;
-	location: string;
+  firstName: string;
+  lastName: string;
+  age: number;
+  location: string;
 }
 const student1: Student = {
-	firstName: "John",
-	lastName: "Doe",
-	age: 20,
-	location: "Lagos",
+  firstName: "John",
+  lastName: "Doe",
+  age: 20,
+  location: "Lagos",
 };
 
 const student2: Student = {
-	firstName: "Jane",
-	lastName: "Doe",
-	age: 22,
-	location: "Abuja",
+  firstName: "Jane",
+  lastName: "Doe",
+  age: 22,
+  location: "Abuja",
 };
 
 const studentsList: Student[] = [student1, student2];
+
+function createTable(students: Student[]): void {
+  const table = document.createElement("table");
+  students.forEach((student) => {
+    const row = table.insertRow();
+    const firstName = row.insertCell();
+    const location = row.insertCell();
+    firstName.innerHTML = student.firstName;
+    location.innerHTML = student.location;
+  });
+    document.body.appendChild(table);
+}
+
+createTable(studentsList);
